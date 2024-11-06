0dae7782bc2b20b7cc0a41ca9a8d954fa6e708d8
ZombieGhostMG <zombieghostmg@gmail.com>
2024-11-06 23:09:33 +0000

    Update SDK structure and generated files

---
+++ index.js
@@ -0,0 +1,5 @@
+export * from './generated/capultTokens';
+export * from './generated/capultEternalVaults';
+export * from './generated/capultTokenLockVaults';
+export * from './generated/capultTokenSales';
+export * from './generated/capultMetadata';
